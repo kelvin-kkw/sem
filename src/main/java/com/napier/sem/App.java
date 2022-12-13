5619352abbf7705e20b3b0c6028bceb5765fb69b
Ko Ko Win <40524303@live.napier.ac.uk>
2022-12-13 23:12:30 +0630

    getEmployee query working  in App.java

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -11,6 +11,12 @@ public class App
 
         // Connect to database
         a.connect();
+        // Get Employee
+        Employee emp = a.getEmployee(255530);
+        System.out.println("Error getting employee ID");
+        // Display results
+        a.displayEmployee(emp);
+        System.out.println("Error displaying employee info");
 
         // Disconnect from database
         a.disconnect();
@@ -124,6 +130,25 @@ public class App
             return null;
         }
     }
+
+
+
+    public void displayEmployee(Employee emp)
+    {
+        if (emp != null)
+        {
+            System.out.println(
+                    emp.emp_no + " "
+                            + emp.first_name + " "
+                            + emp.last_name + "\n"
+                            + emp.title + "\n"
+                            + "Salary:" + emp.salary + "\n"
+                            + emp.dept_name + "\n"
+                            + "Manager: " + emp.manager + "\n");
+        }
+    }
+
+
 
 
 }
