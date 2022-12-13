38329d721423f606ad7eae58b02f037abe77a588
Ko Ko Win <40524303@live.napier.ac.uk>
2022-12-13 21:50:16 +0630

    getEmployee method added in App.java

--- src/main/java/com/napier/sem/Employee.java
+++ src/main/java/com/napier/sem/Employee.java
@@ -1,4 +1,51 @@
 package com.napier.sem;
 
-public class Employee {
+/**
+ * Represents an employee
+ */
+public class Employee
+{
+    /**
+     * Employee number
+     */
+    public int emp_no;
+
+    /**
+     * Employee's first name
+     */
+    public String first_name;
+
+    /**
+     * Employee's last name
+     */
+    public String last_name;
+
+    /**
+     * Employee's job title
+     */
+    public String title;
+
+    /**
+     * Employee's salary
+     */
+    public int salary;
+
+    /**
+     * Employee's current department
+     */
+    public String dept_name;
+
+    /**
+     * Employee's manager
+     */
+    public String manager;
+
+
 }
+
+
+
+
+
+
+
